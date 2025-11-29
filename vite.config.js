686857e10b00dcb0f7053fc2c61ce3371b1139eb
Laissy <laissdominique@gmail.com>
2025-11-29 15:17:30 -0300

    build: set base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_PUBLIC_BASE ?? '/ELLA-store-main/',
   plugins: [
     vue(),
     vueDevTools(),
